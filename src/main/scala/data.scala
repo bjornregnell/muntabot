1ae79d08a5e35c63466029d83ae4935758a9a2c4
bjornregnell <bjorn.regnell@cs.lth.se>
2021-11-25 20:15:02 +0100

    add more data

--- src/main/scala/data.scala
+++ src/main/scala/data.scala
@@ -2,7 +2,24 @@ package muntabot
 
 lazy val terms = Seq[(Week, Concepts | Contrasts | Code)](
   Week(1) -> Concepts("funktion", "while-sats", "for-sats", "stränginterpolator", "tilldelning"),
-  Week(1) -> Contrasts("map" -> "foreach", "for-uttryck" -> "for-sats", "Vector" -> "Array"),
-  Week(2) -> Concepts("for-uttryck", "samlingsmetoden map", "indexering", "kontrollstruktur"),
-  Week(2) -> Code("en funktion som ger minsta talet i en sekvens,\nutan att använda inbyggda min")
+  Week(1) -> Contrasts("map" -> "foreach", "for-uttryck" -> "for-sats", "Vector" -> "Array", "while" -> "for-do"),
+  Week(2) -> Concepts("for-uttryck", "samlingsmetoden map", "indexering", "kontrollstruktur", "huvudprogram"),
+  Week(2) -> Code("en funktion som ger minsta heltalet i en heltalssekvens,\nutan att använda inbyggda min"),
+  Week(2) -> Code("en funktion som ger största heltalet i en heltalssekvens,\nutan att använda inbyggda max"),
+  Week(2) -> Code("två förändringsbara variabler som först initialiseras\nmed två olika helatal och sedan byter värde med varandra"),
+  Week(2) -> Code("en funktion som summerar alla heltal i en sekvens,\nutan att använda inbyggda sum"),
+  Week(2) -> Contrasts("pesudokod" -> "exekverbar implementation", "parameter" -> "argument"),
+  Week(3) -> Concepts("anonym funktion", "predikat", "äkta funktion", "aktiveringspost", "anropsstacken"),
+  Week(4) -> Concepts("singelobjekt"),
+  Week(5) -> Concepts("klass", "attribut", "fabriksmetod"),
+  Week(5) -> Contrasts("funktion" -> "metod"),
+  Week(6) -> Concepts("mönstermatchning", "undantag"),
+  Week(7) -> Contrasts("ArrayBuffer" -> "Array", "förändringsbar samling" -> "oföränderlig samling"),
+  Week(7) -> Code("söker efter en instans av klassen Person\nmed ett visst efternamn i en osorterad sekvens av personer\nmed hjälp av en while (och inte find/indexOf)"),
+  Week(7) -> Code("registrerar 1000 tärningskast i en Array"),
+  Week(8) -> Concepts("matris", "nästlad struktur", "typparameter", "generisk funktion"),
+  Week(8) -> Code("en funktion som skriver ut en strängmatris rad för rad"),
+  Week(9) -> Contrasts("mängd" -> "sekvens", "mängd" -> "nycke-värde-tabell"),
+  Week(10) -> Concepts("arv", "subtypspolymorfism", "bastyp", "final medlem", "privat medlem", "kodgranskning"),
+  Week(10) -> Contrasts("subtyp" -> "supertyp", "klass" -> "trait", "arv" -> "komposition"),
 )
