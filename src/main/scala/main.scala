65ff6ed04de4a152bf0e64bc6bd486f8da4a6c36
oliverlevay <oliver.levay@hotmail.com>
2021-11-30 18:31:00 +0100

    Make "appendButton" actually attach to the "targetNode"

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -17,22 +17,22 @@ def appendButton(targetNode: dom.Node, text: String)(action: => Unit): Unit =
   b.classList.add("button")
   b.appendChild(document.createTextNode(text))
   b.addEventListener("click", (e: dom.Event) => action)
-  document.body.appendChild(b)
+  targetNode.appendChild(b)
 
 
 def setupUI(): Unit = 
   val showText = document.createElement("pre").asInstanceOf[dom.html.Pre]
   showText.textContent = "Klicka så får du en uppgift."
 
-  appendButton(document, "Förklara koncept"){
+  appendButton(document.body, "Förklara koncept"){
     showText.textContent = Concepts.pickAnyQuestion
   }
 
-  appendButton(document, "Jämför koncept"){
+  appendButton(document.body, "Jämför koncept"){
     showText.textContent = Contrasts.pickAnyQuestion
   }
 
-  appendButton(document, "Skriv kod"){
+  appendButton(document.body, "Skriv kod"){
     showText.textContent = Code.pickAnyQuestion
   }
 
