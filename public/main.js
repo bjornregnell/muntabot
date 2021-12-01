4a7824ee51fac7f452afa7921093ad1ed28085f4
oliverlevay <oliver.levay@hotmail.com>
2021-12-01 13:34:48 +0100

    Add helper comments to default public/main.js file

---
+++ public/main.js
@@ -0,0 +1,13 @@
+/**
+ * Generated code goes here!
+ */
+
+/*
+Run commands
+
+  sbt --client fullLinkJS
+  rm public/main.js
+  mv target/scala-3.1.0/muntabot-opt/main.js public/main.js
+
+to generate the javascript and put it here. For more information, look in the readme.
+*/
