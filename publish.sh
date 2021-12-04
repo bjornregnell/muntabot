fe804c5a9dc5ff9da18d2dcc1b334f78a9b77bd3
bjornregnell <bjorn.regnell@cs.lth.se>
2021-12-04 16:38:07 +0100

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-sbt --client fullLinkJS
+sbt fullLinkJS
+cp target/scala-3.1.0/muntabot-opt/main.js public/.
 ssh $LUCATID@fileadmin.cs.lth.se mkdir -p pgk/muntabot
-scp target/scala-3.1.0/muntabot-opt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/muntabot/.
-scp index.html $LUCATID@fileadmin.cs.lth.se:pgk/muntabot/.
+scp public/* $LUCATID@fileadmin.cs.lth.se:pgk/muntabot/.
