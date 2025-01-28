406007c3bf5a4bec9d0d23ef71cc03cf262d2480
bjornregnell <bjorn.regnell@cs.lth.se>
2025-01-28 09:49:48 +0100

    bump version

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 sbt "clean;fullLinkJS"
-cp target/scala-3.5.1/muntabot-opt/main.js public/.
+cp target/scala-3.6.3/muntabot-opt/main.js public/.
 ssh $LUCATID@fileadmin.cs.lth.se mkdir -p pgk/muntabot
 scp public/* $LUCATID@fileadmin.cs.lth.se:pgk/muntabot/.
